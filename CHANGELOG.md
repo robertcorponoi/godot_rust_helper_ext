3d08521d726d1814ae8ba41655e37e0642c5ec30
Robert Corponoi <robertcorponoi@gmail.com>
2020-11-06 15:28:51 -0600

    Updated extensions to work with the latest version of gdnative

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0 / 2020-11-06
+- [FEATURE] Updated extensions to work with latest version of gdnative.
+- [MISC] Updated gdnative dependency to latest version.
+
 ## 0.1.1 / 2020-06-17
 - [MISC] Updated gdnative dependency to use crates.io
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,46 @@
+use gdnative::prelude::{GodotObject, Node, NodePath, Shared, SubClass, TRef};
+
 /// Defines the extension methods that get applied to Nodes.
 pub trait NodeExt {
-	/// Returns a typed node.
-	unsafe fn get_typed_node<T: gdnative::GodotObject, P: Into<gdnative::NodePath>>(&self, path: P) -> Option<T>;
+    /// Returns a typed node with the specified path.
+    unsafe fn get_typed_node<T, P>(&self, path: P) -> TRef<'_, T, Shared>
+    where
+        T: GodotObject + SubClass<Node>,
+        P: Into<NodePath>;
 }
 
-/// Implement the `NodeExt` trait for `Node`.
-impl NodeExt for gdnative::Node {
-	/// Get a node from a provided path and cast it into a specified type.
-	///
-	/// # Arguments
-	///
-	/// `path` - The path to the Node in the Godot project node tree.
-	unsafe fn get_typed_node<T: gdnative::GodotObject, P: Into<gdnative::NodePath>>(&self, path: P) -> Option<T> {
-		self.get_node(path.into()).and_then(|node| node.cast())
-	}
+// /// Implement the `NodeExt` trait for `Node`.
+// impl NodeExt for gdnative::api::Node {
+//     /// Get a node from a provided path and cast it into a specified type.
+//     ///
+//     /// # Arguments
+//     ///
+//     /// `path` - The path to the Node in the Godot project node tree.
+//     unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
+//         self.get_node(path.into()).and_then(|node| {
+//             node.assume_safe();
+//             node.cast::<GodotObject>();
+//         });
+//     }
+// }
+
+// use gdnative::prelude::*;
+
+impl NodeExt for Node {
+    /// Get a node from a provided path and cast it into a specified type.
+    ///
+    /// # Arguments
+    ///
+    /// `path` - The path to the Node in the Godot project node tree.
+    unsafe fn get_typed_node<T, P>(&self, path: P) -> TRef<'_, T, Shared>
+    where
+        T: GodotObject + SubClass<Node>,
+        P: Into<NodePath>,
+    {
+        self.get_node(path.into())
+            .expect("Unable to find node")
+            .assume_safe()
+            .cast()
+            .expect("Unable to cast node to specified type")
+    }
 }
